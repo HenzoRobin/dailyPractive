b2cfdc3ede2615cf28ca43ccb322f7da28ee0b74
Robin <HenzoRobin@github.com>
2018-05-08 18:18:11 +0800

    import components

---
+++ hello/src/components/home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-1 col-xs-offset-11">
+            <h1>HOME</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
