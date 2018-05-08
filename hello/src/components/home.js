d5f0ae4c651c38c150cd2f4135783b861a5065a1
Robin <HenzoRobin@github.com>
2018-05-08 18:32:13 +0800

    tertiary oprator

--- hello/src/components/home.js
+++ hello/src/components/home.js
@@ -8,6 +8,7 @@ class Home extends Component {
         <div className="row">
           <div className="col-xs-1 col-xs-offset-11">
             <h1>HOME</h1>
+            {false ? 'content':'CONTENT'}
           </div>
         </div>
       </div>
